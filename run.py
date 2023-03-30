8e3e7d7129812354c696666ba7a2cb057bee0586
Jan Nogga <jan@nogga.eu>
2023-03-30 16:14:01 +0200

    fix typos

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
 import torch
+
+# custom_add() element-wise adds two tensors
 from lib.module_name import custom_add
 
 A = torch.ones(16).cuda()
 B = 5*A
 
-# custom add is now loaded just-in-time if necessary
+# custom_add is now compiled just-in-time if necessary
 # this is specified by load() in lib.module_name.py
 # in turn, that points to the kernel defined in lib/cuda/dummy.{cpp,cu}
 print(custom_add(A,B))
